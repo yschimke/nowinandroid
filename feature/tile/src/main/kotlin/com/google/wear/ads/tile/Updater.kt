yschimke/cooee-mobile

Initial Wear Commit

feature-tile/src/main/kotlin/ee/coo/mobile/tile/Updater.kt
@@ -1,0 +1,28 @@
+/*
+ * Copyright 2021 Google LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ee.coo.mobile.tile
+
+import android.content.Context
+import androidx.wear.tiles.TileService
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class Updater @Inject constructor(@ApplicationContext val application: Context) {
+    fun update() {
+        TileService.getUpdater(application)
+            .requestUpdate(CooeeTileProviderService::class.java)
+    }
+}

yschimke/nowinandroid

Change-Id: I0aed9c7404827c1c3b1794ccd0d9f7914cb77939

feature/tile/src/main/kotlin/com/google/wear/ads/tile/Updater.kt
@@ -1,0 +1,28 @@
+/*
+ * Copyright 2021 Google LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.wear.ads.tile
+
+import android.content.Context
+import androidx.wear.tiles.TileService
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class Updater @Inject constructor(@ApplicationContext val application: Context) {
+    fun update() {
+        TileService.getUpdater(application)
+            .requestUpdate(AdsTileProviderService::class.java)
+    }
+}